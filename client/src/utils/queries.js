70f6e88311dad8cc9429141742534e1b94f75195
smullock <smullock@hotmail.com>
2023-05-07 12:40:17 +1000

    add queries

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -3,9 +3,29 @@ import { gql } from '@apollo/client';
 export const GET_CATEGORIES = gql`
   query GetCategories {
     categories {
-      id
+      _id
       name
     }
   }
 `;
 
+export const GET_ITEMS = gql`
+  query GetItems {
+    items {
+      _id
+      date
+      city
+      hotel
+      details
+      budgets {
+        category
+        amount
+      }
+      expenses {
+        amount
+      }
+      expenseTotal
+      totalBudget
+    }
+  }
+`;
